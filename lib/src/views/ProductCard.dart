80931484d02a79025f7c0d160ad96a4d15514a5b
Shusmoy <1305108.sc@ugrad.cse.buet.ac.bd>
2021-06-13 17:00:24 +0600

    product card

--- lib/src/views/ProductCard.dart
+++ lib/src/views/ProductCard.dart
@@ -1,88 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:securemart/environments/commonenv.dart';
+import 'package:securemart/models/item.dart';
 
-class ProductTile extends StatelessWidget {
-  final Product product;
-  const ProductTile(this.product);
+class ItemCart extends StatelessWidget {
+  final Item item;
+  final Function(Item i) getItemdetail;
+  const ItemCart(this.item, this.getItemdetail);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: 180,
-                  width: double.infinity,
-                  clipBehavior: Clip.antiAlias,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Image.network(
-                    product.imageLink,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Positioned(
-                  right: 0,
-                  child: Obx(() => CircleAvatar(
-                        backgroundColor: Colors.white,
-                        child: IconButton(
-                          icon: product.isFavorite.value
-                              ? Icon(Icons.favorite_rounded)
-                              : Icon(Icons.favorite_border),
-                          onPressed: () {
-                            product.isFavorite.toggle();
-                          },
+    return InkWell(
+        onTap: () {
+          print(item.itemName);
+          this.getItemdetail(item);
+        },
+        child: Container(
+          width: 200,
+          //height: 200,
+          child: Card(
+            elevation: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        height: 180,
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
                         ),
-                      )),
-                )
-              ],
-            ),
-            SizedBox(height: 8),
-            Text(
-              product.name,
-              maxLines: 2,
-              style:
-                  TextStyle(fontFamily: 'avenir', fontWeight: FontWeight.w800),
-              overflow: TextOverflow.ellipsis,
-            ),
-            SizedBox(height: 8),
-            if (product.rating != null)
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      product.rating.toString(),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 16,
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
+                        child: Image.network(
+                          SERVER_URL + item.thumbnail,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    item.itemName,
+                    maxLines: 2,
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('৳${item.averagePrice}',
+                            style: TextStyle(
+                              fontSize: 15,
+                            )),
+                        IconButton(
+                          onPressed: () {
+                            //print("object");
+                          },
+                          icon: Icon(Icons.shopping_cart),
+                          color: Colors.orange.shade900,
+                        )
+                      ]),
+                ],
               ),
-            SizedBox(height: 8),
-            Text('\$${product.price}',
-                style: TextStyle(fontSize: 32, fontFamily: 'avenir')),
-          ],
-        ),
-      ),
-    );
+            ),
+          ),
+        ));
   }
 }
-
-class Product {}
